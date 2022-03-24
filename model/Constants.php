dfdd8b5b9c02afba694ba0aab44cb27aff4ac576
Kuro020 <52428261+Kuro020@users.noreply.github.com>
2022-03-24 17:13:25 -0300

    Update Constants.php

--- model/Constants.php
+++ model/Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','mateusfortunato.mysql.database.azure.com');
+define('DB_USER','mateus@mateusfortunato');
+define('DB_PASS','senacSBC@123');
 define('DB_NAME','hortfruitbd');
